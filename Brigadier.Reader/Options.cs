cf0ab0ed929707acd5254c832e321123f20f2db0
Bobby Cafazzo <robocafaz@gmail.com>
2015-07-13 21:52:30 -0400

    Adding options file.

---
+++ Brigadier.Reader/Options.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brigadier.Reader
+{
+    public class Options
+    {
+        public const string UserName = "{username}";
+        public const string Password = "{password}";
+        public const string Secret = "{secret}";
+    }
+}
