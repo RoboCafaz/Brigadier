b6675749fc8c9290cb74624e4f6bc3abe2161dc1
Bobby Cafazzo <robocafaz@gmail.com>
2015-07-13 21:28:09 -0400

    Adding subreddit viewer.

---
+++ Brigadier.Web/Controllers/SubredditsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Brigadier.EntityFramework;
+
+namespace Brigadier.Web.Controllers
+{
+    public class SubredditsController : Controller
+    {
+        // GET: Subreddits
+        public ActionResult Index()
+        {
+            using (var context = new BrigadierEntities())
+            {
+                var subs = context.WatchedSubs.OrderBy(x => x.Url).ToList();
+                ViewBag.Title = "Watched Subreddits";
+                return View(subs);
+            }
+        }
+    }
+}
