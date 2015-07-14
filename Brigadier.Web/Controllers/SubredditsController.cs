81344bea3d60278a05289b27f44209f6a86eb3c9
Bobby Cafazzo <robocafaz@gmail.com>
2015-07-13 20:59:18 -0400

    Adding jQuery and Bootstrap, yo.

---
+++ Brigadier.Web/Controllers/SubredditsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Brigadier.Web.Controllers
+{
+    public class SubredditsController : Controller
+    {
+        // GET: Subreddits
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
