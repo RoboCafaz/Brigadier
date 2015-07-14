a33ea46ae20c2d166940ea67cbe48e965616536b
Bobby Cafazzo <robocafaz@gmail.com>
2015-07-13 22:20:37 -0400

    Using user authentication for now.

---
+++ Brigadier.Reader.Runner/Runner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Brigadier.Reader.Analyzer;
+
+namespace Brigadier.Reader.Runner
+{
+    public class Runner
+    {
+        public static void Main(String[] args)
+        {
+            SubReader.Run();
+        }
+    }
+}
--- Brigadier.Reader/Analyzer/Subreader.cs
+++ Brigadier.Reader/Analyzer/Subreader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Brigadier.EntityFramework;
 using RedditSharp;
 
 namespace Brigadier.Reader.Analyzer
@@ -14,15 +15,23 @@ namespace Brigadier.Reader.Analyzer
             var reddit = GetReddit();
         }
 
-        public static Reddit GetReddit()
+        private static Reddit GetReddit()
         {
-            var reddit = new Reddit(Options.Secret);
+            var reddit = new Reddit(Options.UserName, Options.Password);
             reddit.InitOrUpdateUser();
             if (reddit.User == null)
             {
                 throw new RedditException("Could not authenticate user!");
             }
             return reddit;
+        }
+
+        private static void CheckSubs()
+        {
+            using (var context = new BrigadierEntities())
+            {
+                
+            }
         }
     }
 }
