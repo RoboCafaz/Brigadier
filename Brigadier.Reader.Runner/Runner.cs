1f320a7d646b7269afdfadfe05155c6b452a64a1
Bobby Cafazzo <robocafaz@gmail.com>
2015-07-17 02:02:33 -0400

    Preparing reader for webjob publishing.

--- Brigadier.Reader.Runner/Runner.cs
+++ Brigadier.Reader.Runner/Runner.cs
@@ -11,7 +11,7 @@ namespace Brigadier.Reader.Runner
     {
         public static void Main(String[] args)
         {
-            //SubReader.Run();
+            SubReader.Run();
             PostReader.Run();
         }
     }
