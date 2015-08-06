6d9b5d6692ae072de14f2789541fd2ebaff7e3e1
Bobby Cafazzo <robocafaz@gmail.com>
2015-08-05 20:25:25 -0400

    Adding generic stats page.

---
+++ Brigadier.Web/Controllers/StatsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web;
+using System.Web.Mvc;
+using Brigadier.EntityFramework;
+using Brigadier.Web.Models;
+
+namespace Brigadier.Web.Controllers
+{
+    public class StatsController : Controller
+    {
+        // GET: Subreddits
+        public ActionResult Index()
+        {
+            using (var context = new BrigadierEntities())
+            {
+                var posts = context.Posts.GroupBy(x => x.LocalThread.Author).Select(x => new UserPostCountModel
+                {
+                    Name = x.Key,
+                    Count = x.Count(),
+                    SubPosts = x.GroupBy(y => y.LocalThread.Sub).Select(y => new PostCountModel
+                    {
+                        Name = y.Key,
+                        Count = y.Count()
+                    }).OrderByDescending(y => y.Count)
+                }).OrderByDescending(x => x.Count);
+                return View(posts.ToList());
+            }
+        }
+    }
+}
---
+++ Brigadier.Web/Models/UserPostModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Brigadier.Web.Models
+{
+    public class PostCountModel
+    {
+        public String Name { get; set; }
+        public int Count { get; set; }
+    }
+
+    public class UserPostCountModel : PostCountModel
+    {
+        public UserPostCountModel()
+        {
+            SubPosts = new List<PostCountModel>();
+        }
+
+        public IEnumerable<PostCountModel> SubPosts { get; set; }
+    }
+}
